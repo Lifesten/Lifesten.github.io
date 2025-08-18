779b0fdab144f67948e068dccdd0d2656ddbaa79
sharad <sharadvadgama9@gmail.com>
2025-08-18 21:10:51 +0530

    version 9.0 dashboard ui related updated.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -153,7 +153,7 @@ const RESOURCES = {"assets/assets/jsons/credentials_calendar.json": "3516aca4317
 "assets/AssetManifest.json": "08036164a0c44a71a7bc7318d0b617e6",
 "assets/NOTICES": "881d3c2e6de9ad065a9c05f5cdf0c6a7",
 "favicon.png": "4225cb13e1d3cbd0208d75bcc30b5dd2",
-"main.dart.js": "b86ca6add9e6b40f5236d5073fa96dfd",
+"main.dart.js": "13cc58b6bca328ba75f2a71311d74b5f",
 "icons/Icon-192.png": "d96862abe025aad67d9fc7bd05f1ab38",
 "icons/Icon-maskable-192.png": "d96862abe025aad67d9fc7bd05f1ab38",
 "icons/Icon-512.png": "d80d5c49cce2971609f757b423cf52f8",
@@ -170,8 +170,8 @@ const RESOURCES = {"assets/assets/jsons/credentials_calendar.json": "3516aca4317
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
 "canvaskit/canvaskit.js.symbols": "38cba9233b92472a36ff011dc21c2c9f",
 "version.json": "2895eecf5d892bfcd8a7d77755209568",
-"index.html": "ff3d2c13beba089a31e57612f759d94a",
-"/": "ff3d2c13beba089a31e57612f759d94a",
+"index.html": "1e067fb474002276a2728ca4bb5cd750",
+"/": "1e067fb474002276a2728ca4bb5cd750",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a"};
 // The application shell files that are downloaded before a service worker can
 // start.
