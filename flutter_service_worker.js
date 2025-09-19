23a6213a8727b9ee13b9b19b2b31b176022ac656
sharad <sharadvadgama9@gmail.com>
2025-09-19 11:49:44 +0530

    version 13.1 insurance and workplace mini admin issue solved.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -153,7 +153,7 @@ const RESOURCES = {"assets/assets/jsons/credentials_calendar.json": "3516aca4317
 "assets/AssetManifest.json": "08036164a0c44a71a7bc7318d0b617e6",
 "assets/NOTICES": "881d3c2e6de9ad065a9c05f5cdf0c6a7",
 "favicon.png": "4225cb13e1d3cbd0208d75bcc30b5dd2",
-"main.dart.js": "f3298556ab94c77f451eb13b7f59eeaa",
+"main.dart.js": "ad3d1c7e9919c84bb681ae8bc2df51f6",
 "icons/Icon-192.png": "d96862abe025aad67d9fc7bd05f1ab38",
 "icons/Icon-maskable-192.png": "d96862abe025aad67d9fc7bd05f1ab38",
 "icons/Icon-512.png": "d80d5c49cce2971609f757b423cf52f8",
@@ -170,8 +170,8 @@ const RESOURCES = {"assets/assets/jsons/credentials_calendar.json": "3516aca4317
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
 "canvaskit/canvaskit.js.symbols": "38cba9233b92472a36ff011dc21c2c9f",
 "version.json": "2895eecf5d892bfcd8a7d77755209568",
-"index.html": "a6c10a422da70f764a027a0d1401b98e",
-"/": "a6c10a422da70f764a027a0d1401b98e",
+"index.html": "404a26005ef4f2ca343b2137ca43ac38",
+"/": "404a26005ef4f2ca343b2137ca43ac38",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a"};
 // The application shell files that are downloaded before a service worker can
 // start.
